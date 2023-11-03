28ef9139dbcbf2563d056d9970b0e17500dbc245
xiaodeng <1342677061@qq.com>
2023-11-03 14:52:56 +0800

    er

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+import { Button, message, ConfigProvider } from 'antd';
+import Login from './pages/login'
+import Admin from './pages/admin'
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <ConfigProvider theme={{ token: { colorPrimary: '#00b96b' } }}>
+
+      <Routes>{/* 只匹配某一个路由*/}
+        <Route path='/login' Component={Login}></Route>
+        <Route path='/' Component={Admin}></Route>
+      </Routes>
+
+    </ConfigProvider>
   );
 }
 
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './login.less'
+//登录的路由界面
+export default class Login extends Component {
+  render() {
+    return (
+      <div className='login'>
+        <div className='login-header'></div>
+        <div className='login-content'></div>
+      </div>
+    )
+  }
+}
